ff0a4f2872d148175c71832020c8771849f80e07
bhargavi-gummanur <bhargavi.gummanur@hpe.com>
2022-06-23 14:50:24 +0530

    testing v5

--- example_dags/dag_test.py
+++ example_dags/dag_test.py
@@ -21,7 +21,7 @@ default_args = {
 }
 
 dag = DAG(
-    'kubernetes_sample', default_args=default_args,
+    'kubernetes_sample_testv2', default_args=default_args,
     schedule_interval=timedelta(minutes=10), tags=['example'])
 
 start = DummyOperator(task_id='run_this_first', dag=dag)
