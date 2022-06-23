7faaff4677c70cb7baed76fc57fc70bab3131b38
bhargavi-gummanur <bhargavi.gummanur@hpe.com>
2022-06-23 19:20:50 +0530

    testing v13

--- example_dags/dag_test.py
+++ example_dags/dag_test.py
@@ -1,6 +1,7 @@
 import logging
 
 from airflow import DAG
+import airflow.models.taskinstance as task
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import \
     KubernetesPodOperator
 from airflow.operators.dummy_operator import DummyOperator
@@ -8,13 +9,18 @@ from datetime import datetime, timedelta
 
 # log = logging.getLogger(__name__)
 
-def return_hello_world(s):
-    print(s)
+def return_hello_world(**context):
+    
     params = {
         "1":"one",
         "2":"two"
     }
+    context['task_instance'].xcom_push(key='pushing params',value = params)
     return params
+def pull_xcom(**kwargs):
+    ti = kwargs['task_instance']
+    params = ti.xcom_pull(task_ids = 'passing-task-python',key = 'pushing params')
+    print(params)
 
 def print_another(params):
     print(params)
@@ -39,7 +45,7 @@ start = DummyOperator(task_id='run_this_first', dag=dag)
 python_task = KubernetesPodOperator(namespace='default',
                                     image="python:3.6",
                                     #cmds=["python", "-c"],
-                                    arguments=['echo \'{}\' > /airflow/xcom/return.json'.format(return_hello_world("hi"))],
+                                    arguments=['echo \'{}\' > /airflow/xcom/return.json'.format(return_hello_world())],
                                     labels={"foo": "bar"},
                                     name="passing-python",
                                     task_id="passing-task-python",
@@ -51,8 +57,8 @@ python_task = KubernetesPodOperator(namespace='default',
 
 bash_task = KubernetesPodOperator(namespace='default',
                                     image="python:3.6",
-                                    cmds=["python", "-c"],
-                                    arguments=[{{task_instance.xcom_pull('passing-task-python')}}],
+                                    #cmds=["python", "-c"],
+                                    arguments=[pull_xcom],
                                     labels={"foo": "bar"},
                                     name="passing-python1",
                                     task_id="passing-task-python1",
