b4e9fed237495c837d03161b3de983951b95b744
bhargavi-gummanur <bhargavi.gummanur@hpe.com>
2022-06-24 12:37:52 +0530

    testing v28

--- example_dags/dag_test.py
+++ example_dags/dag_test.py
@@ -1,40 +1,17 @@
+"""
+This is an example dag for using the KubernetesPodOperator.
+"""
 import logging
-import json
+
 from airflow import DAG
-import os
-import airflow.models.taskinstance as task
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import \
     KubernetesPodOperator
 from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators.bash_operator import BashOperator
 from datetime import datetime, timedelta
 
 # log = logging.getLogger(__name__)
 
-def return_hello_world(**context):
-    
-    params = {
-        "1":"one",
-        "2":"two"
-    }
-    '''
-    save_path = "/airflow/xcom"
-    filename = "return.json"
-    comp = os.path.join(save_path,filename)
-    #context['task_instance'].xcom_push(key='pushing params',value = params)
-    json_file = open(filename, 'w')
-    json_file.write('params')
-    json_file.close()
-    '''
-    return "print('hello world')"
-def pull_xcom(**kwargs):
-    ti = kwargs['task_instance']
-    params = ti.xcom_pull(task_ids = 'passing-task-python')
-    print(params)
 
-def print_another(params):
-    print(params)
-    
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
@@ -43,39 +20,38 @@ default_args = {
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
-    'retry_delay': timedelta(minutes=5),
-    'provide_context' : True
+    'retry_delay': timedelta(minutes=5)
 }
 
 dag = DAG(
-    'kubernetes_sample_testv2', default_args=default_args,
+    'kubernetes_sample', default_args=default_args,
     schedule_interval=timedelta(minutes=10), tags=['example'])
 
 start = DummyOperator(task_id='run_this_first', dag=dag)
 
-write_xcom = KubernetesPodOperator(
-        namespace='default',
-        image='alpine',
-        cmds = ["python","-c"],
-        arguments = [return_hello_world()],
-        #cmds=["sh", "-c", "mkdir -p /airflow/xcom/;echo '[1,2,3,4]' > /airflow/xcom/return.json"],
-        name="write-xcom",
-        #do_xcom_push=True,
-        is_delete_operator_pod=True,
-        in_cluster=True,
-        task_id="write-xcom",
-        get_logs=True,
-        dag=dag,
-)
+python_task = KubernetesPodOperator(namespace='default',
+                                    image="python:3.6",
+                                    cmds=["python", "-c"],
+                                    arguments=["print('hello world')"],
+                                    labels={"foo": "bar"},
+                                    name="passing-python",
+                                    task_id="passing-task-python",
+                                    get_logs=True,
+                                    do_xcom_push = True,
+                                    dag=dag
+                                    )
 
-pod_task_xcom_result = BashOperator(
-        bash_command="echo \"{{ task_instance.xcom_pull('write-xcom')[0] }}\"",
-        task_id="pod_task_xcom_result",
-        dag=dag,
-)
+bash_task = KubernetesPodOperator(namespace='default',
+                                  image="ubuntu:16.04",
+                                  cmds=["bash", "-cx"],
+                                  arguments=["date"],
+                                  labels={"foo": "bar"},
+                                  name="passing-bash",
+                                  # is_delete_operator_pod=False,
+                                  task_id="passing-task-bash",
+                                  get_logs=True,
+                                  dag=dag
+                                  )
 
-write_xcom >> pod_task_xcom_result
-
-#python_task.set_upstream(start)
-#bash_task.set_upstream(start)
-#start >> python_task >> bash_task
+python_task.set_upstream(start)
+bash_task.set_upstream(start)
