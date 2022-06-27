ceca81c917685008fb5d0db9c814ee5a57b3d3c4
bhargavi-gummanur <bhargavi.gummanur@hpe.com>
2022-06-27 18:03:46 +0530

    testing v36

--- example_dags/example_kubernetes.py
+++ example_dags/example_kubernetes.py
@@ -49,7 +49,7 @@ with DAG(
 
     k = KubernetesPodOperator(
         namespace='airflowop-system',
-        image="glmlopsuser/my-airflow-python:0.1",
+        image="glmlopsuser//airflow-metadata:0.1",
         image_pull_secrets=[k8s.V1LocalObjectReference('airflow-metadata1')],
         cmds=["python"],
         arguments=['print("hi")'],
