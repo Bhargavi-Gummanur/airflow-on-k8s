4a5766d2f5dc027807830ad6da638920630cf05f
bhargavi-gummanur <bhargavi.gummanur@hpe.com>
2022-06-27 15:09:00 +0530

    testing v36

--- example_dags/example_kubernetes.py
+++ example_dags/example_kubernetes.py
@@ -48,7 +48,7 @@ with DAG(
     ]
 
     k = KubernetesPodOperator(
-        namespace='rakeshl-test',
+        namespace='airflow',
         image="ubuntu:16.04",
         cmds=["bash", "-cx"],
         arguments=["echo hello here"],
