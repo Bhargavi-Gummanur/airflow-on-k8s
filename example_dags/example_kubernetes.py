ca053562b17d8486d1c72291a97804b93a785016
bhargavi-gummanur <bhargavi.gummanur@hpe.com>
2022-06-28 12:39:34 +0530

    testing v36

--- example_dags/example_kubernetes.py
+++ example_dags/example_kubernetes.py
@@ -1,29 +1,9 @@
-#
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
-"""
-This is an example dag for using the KubernetesPodOperator.
-"""
 import logging
 
 from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 from airflow.utils.dates import days_ago
-from kubernetes.client import models as k8s
+
 log = logging.getLogger(__name__)
 
 
@@ -48,11 +28,10 @@ with DAG(
     ]
 
     k = KubernetesPodOperator(
-        namespace='airflowop-system',
-        image="python:3.8",
-        #image_pull_secrets=[k8s.V1LocalObjectReference('airflow-metadata1')],
-        cmds=["python"],
-        arguments=['print("hi")'],
+        namespace='default',
+        image="ubuntu:16.04",
+        cmds=["bash", "-cx"],
+        arguments=["echo hello here"],
         labels={"foo": "bar"},
         name="airflow-test-pod",
         task_id="task",
