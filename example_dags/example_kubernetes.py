9404753bb5d3999817638ec1022437bea837e3bb
bhargavi-gummanur <bhargavi.gummanur@hpe.com>
2022-06-29 19:27:00 +0530

    testing v36

--- example_dags/example_kubernetes.py
+++ example_dags/example_kubernetes.py
@@ -3,7 +3,7 @@ import logging
 from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 from airflow.utils.dates import days_ago
-
+from datetime import datetime, timedelta
 log = logging.getLogger(__name__)
 
 
