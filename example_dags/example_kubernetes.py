a4e9e04182d498e45905fbb5709e57f7c71222ef
bhargavi-gummanur <bhargavi.gummanur@hpe.com>
2022-06-29 18:36:15 +0530

    testing v36

--- example_dags/example_kubernetes.py
+++ example_dags/example_kubernetes.py
@@ -3,13 +3,13 @@ import logging
 from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 from airflow.utils.dates import days_ago
-from datetime import datetime, timedelta
+
 log = logging.getLogger(__name__)
 
 
 default_args = {
     'owner': 'airflow',
-    'start_date': datetime(1970,1,1)
+    'start_date': days_ago(2)
 }
 
 with DAG(
@@ -27,24 +27,15 @@ with DAG(
         }
     ]
 
-    '''
+    k = KubernetesPodOperator(
+        namespace='default',
         image="ubuntu:16.04",
         cmds=["bash", "-cx"],
         arguments=["echo hello here"],
         labels={"foo": "bar"},
         name="airflow-test-pod",
-        node_selectors={"kubernetes.io/hostname": "gl1-cp-tr-node1.gl-hpe.local"},
-    '''
-    #namespace="airflowop-system",
-    #pod_template_file="/usr/local/airflow/dags/gitdags/example_dags/example-python.yaml",
-    k = KubernetesPodOperator(
-        labels={"foo": "bar"},
-        cmds=["bash", "-cx"],
-        arguments=["echo hello here"],
         task_id="task",
-        pod_template_file="./example-python.yaml",
         get_logs=True,
-        is_delete_operator_pod=False
-       
-        #tolerations=tolerations
+        is_delete_operator_pod=False,
+        tolerations=tolerations
     )
