9b8942b1ee4bb0a8acbc3eca5b278089e30fba83
bhargavi-gummanur <bhargavi.gummanur@hpe.com>
2022-06-30 10:58:53 +0530

    testing v36

--- example_dags/example_kubernetes.py
+++ example_dags/example_kubernetes.py
@@ -5,7 +5,7 @@ from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import Kubernete
 from airflow.utils.dates import days_ago
 from datetime import datetime, timedelta
 log = logging.getLogger(__name__)
-
+from kubernetes.client import models as k8s
 
 default_args = {
     'owner': 'airflow',
