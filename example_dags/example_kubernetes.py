2544d11928b7b88958e908252ee654a5a1edd815
bhargavi-gummanur <bhargavi.gummanur@hpe.com>
2022-07-01 13:17:25 +0530

    testing v36

--- example_dags/example_kubernetes.py
+++ example_dags/example_kubernetes.py
@@ -2,7 +2,6 @@ import logging
 
 from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
-from airflow.operators.bash_operator import BashOperator
 from airflow.utils.dates import days_ago
 from datetime import datetime, timedelta
 log = logging.getLogger(__name__)
@@ -29,21 +28,14 @@ with DAG(
         }
     ]
     resource_config = {'limit_memory': '1024Mi', 'limit_cpu': '500m'}
-    return_value = {"hi":"1"}
     k = KubernetesPodOperator(
         namespace='rakeshl-test',
         image="glmlopsuser/my-airflow-python:0.2",
         image_pull_secrets=[k8s.V1LocalObjectReference('airflow-secretv2')],
-        cmds=["sh", "-c", "mkdir -p /airflow/xcom/;echo '[1,2,3,4]' > /airflow/xcom/return.json"],
-        #arguments=['echo \'{}\' > /airflow/xcom/return.json'.format(return_value)],
+        cmds=["python"],
+        arguments=['task1.py','print("helloarguements")'],
         resources=resource_config,
         name="airflow-test-pod",
         task_id="task",
-        do_xcom_push=True
         
     )
-    pod_task_xcom_result = BashOperator(
-        bash_command="echo \"{{ task_instance.xcom_pull('write-xcom')[0] }}\"",
-        task_id="pod_task_xcom_result",
-    )
-    k >> pod_task_xcom_result
